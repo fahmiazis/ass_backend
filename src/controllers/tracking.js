28e41a701925992ed129f681577d5b86452b500b
fahmiazis <fahmiazis797@gmail.com>
2022-02-02 14:24:10 +0700

    update tracking

--- src/controllers/tracking.js
+++ src/controllers/tracking.js
@@ -1,6 +1,6 @@
-const { disposal, path, ttd } = require('../models')
+const { disposal, path, ttd, mutasi, asset } = require('../models')
 const response = require('../helpers/response')
-const { Op } = require('sequelize')
+// const { Op } = require('sequelize')
 
 module.exports = {
   getTracking: async (req, res) => {
@@ -41,6 +41,49 @@ module.exports = {
       const noDis = [...set]
       return response(res, 'success get tracking', { result, noDis })
     } else {
+      return response(res, 'failed get tracking', {}, 404, false)
+    }
+  },
+  getTrackMutasi: async (req, res) => {
+    try {
+      const kode = req.user.kode
+      const result = await mutasi.findAndCountAll({
+        where: {
+          kode_plant: kode
+        },
+        include: [
+          {
+            model: ttd,
+            as: 'appForm'
+          },
+          {
+            model: path,
+            as: 'pict'
+          },
+          {
+            model: asset,
+            as: 'dataAsset'
+          }
+        ],
+        order: [
+          ['id', 'ASC'],
+          [{ model: ttd, as: 'appForm' }, 'id', 'DESC']
+        ]
+      })
+      if (result) {
+        const data = []
+        result.rows.map(x => {
+          return (
+            data.push(x.no_mutasi)
+          )
+        })
+        const set = new Set(data)
+        const noMut = [...set]
+        return response(res, 'success get tracking', { result, noMut })
+      } else {
+        return response(res, 'failed to get tracking', {}, 404, false)
+      }
+    } catch (error) {
       return response(res, 'failed get tracking', {}, 404, false)
     }
   }
--- src/routes/tracking.js
+++ src/routes/tracking.js
@@ -2,5 +2,6 @@ const route = require('express').Router()
 const track = require('../controllers/tracking')
 
 route.get('/get', track.getTracking)
+route.get('/mutasi', track.getTrackMutasi)
 
 module.exports = route
