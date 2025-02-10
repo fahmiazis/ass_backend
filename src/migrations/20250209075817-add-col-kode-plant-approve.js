c8f05d0bd8f94837555773fd2f384b04cc81cf4c
fahmiazis <fahmiazis797@gmail.com>
2025-02-10 09:13:38 +0700

    modified:   src/migrations/20250209075817-add-col-kode-plant-approve.js

--- src/migrations/20250209075817-add-col-kode-plant-approve.js
+++ src/migrations/20250209075817-add-col-kode-plant-approve.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('nameapproves', 'tipe', {
+    await queryInterface.addColumn('nameApproves', 'tipe', {
       type: Sequelize.DataTypes.STRING
     })
-    await queryInterface.addColumn('nameapproves', 'kode_plant', {
+    await queryInterface.addColumn('nameApproves', 'kode_plant', {
       type: Sequelize.DataTypes.STRING
     })
     await queryInterface.addColumn('approves', 'access', {
@@ -23,8 +23,8 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('nameapproves', 'tipe', {})
-    await queryInterface.removeColumn('nameapproves', 'kode_plant', {})
+    await queryInterface.removeColumn('nameApproves', 'tipe', {})
+    await queryInterface.removeColumn('nameApproves', 'kode_plant', {})
     await queryInterface.removeColumn('approves', 'access', {})
     await queryInterface.removeColumn('approves', 'status', {})
     await queryInterface.removeColumn('approves', 'tipe', {})
