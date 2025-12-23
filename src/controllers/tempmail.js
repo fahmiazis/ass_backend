d6e01793703535f424926101da2095cda868f793
fahmiazis <fahmiazis797@gmail.com>
2025-12-23 16:52:19 +0700

    modified:   src/controllers/tempmail.js

--- src/controllers/tempmail.js
+++ src/controllers/tempmail.js
@@ -2366,7 +2366,7 @@ module.exports = {
               from: 'noreply_aset@pinusmerahabadi.co.id',
               replyTo: 'noreply_aset@pinusmerahabadi.co.id',
               to: `${to}`,
-              cc: `${cc.split(',')}, noreplyofr@gmail.com, yuda.firmansyah@pinusmerahabadi.co.id, neng_rina@pinusmerahabadi.co.id`,
+              cc: `${cc.split(',')}, noreplyofr@gmail.com, yuda.firmansyah@pinusmerahabadi.co.id, neng_rina@pinusmerahabadi.co.id, pmaho_asset1@pinusmerahabadi.co.id, pmaho_asset2@pinusmerahabadi.co.id`,
               // to: 'neng_rina@pinusmerahabadi.co.id',
               // cc: 'pmaho_asset1@pinusmerahabadi.co.id, fahmi_aziz@pinusmerahabadi.co.id, noreplyofr@gmail.com',
               // to: 'noreplyofr@gmail.com',
